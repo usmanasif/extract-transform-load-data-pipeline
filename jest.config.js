1c1cf5465d79b081effb6c3baa75fb786732ac32
Usman Asif <uasif.work.mail@gmail.com>
2023-02-23 04:53:12 +0500

    Project Setup

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Sync object
+const config = {
+  verbose: true,
+  transform: {
+    "^.+\\.tsx?$": "ts-jest",
+  },
+  testPathIgnorePatterns: ["/node_modules/", "/build"],
+};
+exports.default = config;
